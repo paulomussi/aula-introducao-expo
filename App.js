9856f0e4f208902c533e42d0b3bf08dccf25b02d
paulomussi <127447854+paulomussi@users.noreply.github.com>
2025-08-22 21:13:24 -0300

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Exemplo01 from './src/exemplos/ex01';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Exemplo01 />
       <StatusBar style="auto" />
     </View>
   );
