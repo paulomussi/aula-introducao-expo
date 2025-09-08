cd205f3513ebab3d9850fbd5c54907a04fce3e2b
paulomussi <127447854+paulomussi@users.noreply.github.com>
2025-09-08 19:20:12 -0300

    Update styles.js

--- src/atividades/atv02/card/styles.js
+++ src/atividades/atv02/card/styles.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
         color: '#fafafa',
         
     },
-     containerImage:{
+     containerImagem:{
         width: '30%',
         resizeMode: 'contain',
      },
