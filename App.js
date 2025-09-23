d8a39e831af4a72e106334b8ce4e5490c32a0ca0
PAULO RICARDO MUSSI <paulo.mussi>
2025-09-22 22:08:56 -0300

    atividade4

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#15105cff',
+    backgroundColor: '#613860',
     // alignItems: 'center',
     // justifyContent: 'center',
     padding: RFPercentage(1.5),  
--- src/atividades/atv04/styles.js
+++ src/atividades/atv04/styles.js
@@ -7,46 +7,52 @@ const styles = StyleSheet.create(
     {
         container: {
             flex: 1,
-            backgroundColor: 'rgba(227, 224, 245, 1)',
+            backgroundColor: '#536c8da2',
             alignItems: 'center',
         },
         titulo: {
             fontSize: RFPercentage(5),
             // fontSize: RFValue(25),
             fontWeight: 'bold',
-            color: '#5a5072ff',
-            margin: RFPercentage(5),
+            color: '#62a07b',
+            margin: RFPercentage(6),
+            marginTop: RFPercentage(5),
         },
         txt:{
             fontSize: RFPercentage(2.5),
         },
         input:{
             borderWidth: RFPercentage(0.5),
-            borderColor: '#5a5072ff',
+            borderColor: '#62a07b',
             width: '80%',
             borderRadius: RFPercentage(1.5),
             padding: RFPercentage(1.5),
             fontSize: RFPercentage(2),
-            textAlign: 'center'
+            textAlign: 'center',
         },
         botao:{
-            color:'rgba(15, 5, 51, 1)',
+            color:'#62a07b',
             fontSize: RFPercentage(2),
             padding: RFPercentage(1),
             borderRadius: RFPercentage(2),
             borderWidth: RFPercentage(0.2),
             margin: RFPercentage(2.6),
-            borderColor: '#5a5072ff',
+            borderColor: '#62a07b',
             width: '80%',
             alignItems: 'center',
-            backgroundColor:'#5a5072ff',
+            backgroundColor:'#62a07b',
         },
         txtBotao:{
             fontSize: RFPercentage(2.5),
             color: '#fafafa',
         },
         txtNomeSobre:{
-            FontSize: RFPercentage(2),
+            fontSize: RFPercentage(3),
+            color: '#613860',
+            marginTop : RFPercentage(4),
+            margin: RFPercentage(1),
+            textAlign: 'left',
+            
                   
         },
     }
