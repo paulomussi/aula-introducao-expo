46bc2eee88d004040058a5eee3265d4c654af1fc
paulomussi <127447854+paulomussi@users.noreply.github.com>
2025-09-29 20:07:34 -0300

    v

--- App.js
+++ App.js
@@ -8,17 +8,19 @@ import Exemplo01 from './src/exemplos/ex01';
 import Exemplo02 from './src/exemplos/ex02';
 import Exemplo03 from './src/exemplos/ex03';
 import Exemplo04 from './src/exemplos/ex04';
+import Exemplo05 from './src/exemplos/ex05';
 
 
 import Atividade01 from './src/atividades/atv01';
 import Atividade02 from './src/atividades/atv02';
 import Atividade03 from './src/atividades/atv03';
 import Atividade04 from './src/atividades/atv04';
+import Atividade05 from './src/atividades/atv04';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade04/>
+      <Exemplo05/>
       <StatusBar style="auto" />
     </View>
   );
--- src/exemplos/ex04/styles.js
+++ src/exemplos/ex04/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet, TextInput } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 
@@ -7,43 +7,29 @@ const styles = StyleSheet.create(
     {
         container: {
             flex: 1,
-            backgroundColor: 'rgba(227, 224, 245, 1)',
+            backgroundColor: '#ff1',
             alignItems: 'center',
+            justifyContent: 'space-evenly',
         },
         titulo: {
             fontSize: RFPercentage(5),
             // fontSize: RFValue(25),
             fontWeight: 'bold',
-            color: '#5a5072ff',
-            margin: RFPercentage(5),
+            color: '#777',
         },
         txt:{
             fontSize: RFPercentage(2.5),
         },
-        input:{
-            borderWidth: RFPercentage(0.5),
-            borderColor: '#5a5072ff',
-            width: '80%',
-            borderRadius: RFPercentage(1.5),
-            padding: RFPercentage(1.5),
-            fontSize: RFPercentage(2),
-            textAlign: 'center'
-        },
         botao:{
-            color:'rgba(15, 5, 51, 1)',
-            fontSize: RFPercentage(2),
+            backgroundColor: '#22a2d4ff',
+            width: '60%',
+            borderRadius: RFPercentage(1),
             padding: RFPercentage(1),
-            borderRadius: RFPercentage(2),
-            borderWidth: RFPercentage(0.2),
-            margin: RFPercentage(2.6),
-            borderColor: '#5a5072ff',
-            width: '80%',
-            alignItems: 'center',
-            backgroundColor:'#5a5072ff',
+            alignItems:'center',
         },
         txtBotao:{
-            fontSize: RFPercentage(2.5),
-            color: '#fafafa',
+            color:'rgba(26, 21, 95, 1)',
+            fontSize: RFPercentage(2),
         },
     }
 );
